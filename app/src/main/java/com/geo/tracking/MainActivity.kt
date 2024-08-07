aad7de5978bae01efd39f7010596f48d533f02b6
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-07 23:13:27 +0630

    design location text ui

--- app/src/main/java/com/geo/tracking/MainActivity.kt
+++ app/src/main/java/com/geo/tracking/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.geo.tracking
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.geo.tracking.ui.theme.GeoTrackingTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +32,94 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GeoTrackingTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    var locationText by remember { mutableStateOf("No location obtained :(") }
+                    var showPermissionResultText by remember { mutableStateOf(false) }
+                    var permissionResultText by remember { mutableStateOf("Permission Granted...") }
+
+                    RequestLocationPermission(
+                        onPermissionGranted = {
+                            showPermissionResultText = true
+                        },
+                        onPermissionDenied = {
+                            showPermissionResultText = true
+                            permissionResultText = "Permissoin Denied :("
+                        },
+                        onPermissionsRevoked = {
+                            showPermissionResultText = true;
+                            permissionResultText = "Permission Revoked :("
+                        }
                     )
+
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Requesting location permission...",
+                            textAlign = TextAlign.Center
+                        )
+                        if (showPermissionResultText) {
+                            Text(text = permissionResultText, textAlign = TextAlign.Center)
+                            Text(text = locationText, textAlign = TextAlign.Center)
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun RequestLocationPermission(
+    onPermissionGranted: () -> Unit,
+    onPermissionDenied: () -> Unit,
+    onPermissionsRevoked: () -> Unit
+) {
+    val permissionState = rememberMultiplePermissionsState(
+        listOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
     )
+    LaunchedEffect(key1 = permissionState) {
+        val allPermissionRevoked =
+            permissionState.permissions.size == permissionState.revokedPermissions.size
+
+        val permissionToRequest = permissionState.permissions.filter { !it.status.isGranted }
+
+        if (permissionToRequest.isNotEmpty()) permissionState.launchMultiplePermissionRequest()
+
+        if (allPermissionRevoked) {
+            onPermissionsRevoked()
+        } else {
+            if (permissionState.allPermissionsGranted) {
+                onPermissionGranted()
+            } else {
+                onPermissionDenied()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GeoTrackingTheme {
-        Greeting("Android")
+        RequestLocationPermission(
+            onPermissionGranted = {
+
+            },
+            onPermissionDenied = {
+
+            },
+            onPermissionsRevoked = {
+
+            }
+        )
     }
 }
