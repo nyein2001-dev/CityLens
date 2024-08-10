8855b22416700d9ed3cefcab99cd3171e39ba2e1
nyeinchantoe-dev <nyeinchantoe0@gmail.com>
2024-08-10 15:29:02 +0630

    request location

--- app/src/main/java/com/geo/tracking/MainActivity.kt
+++ app/src/main/java/com/geo/tracking/MainActivity.kt
@@ -1,28 +1,46 @@
 package com.geo.tracking
 
 import android.os.Bundle
+import android.Manifest
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import com.geo.tracking.extension.hasLocationPermission
 import com.geo.tracking.ui.theme.GeoTrackingTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+
+            val permissionState = rememberMultiplePermissionsState(
+                permissions = listOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+
             GeoTrackingTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    LaunchedEffect(!hasLocationPermission()) {
+                        permissionState.launchMultiplePermissionRequest()
+                    }
                     Text(textAlign = TextAlign.Center, text = "Main Screen")
                 }
             }
